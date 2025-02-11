8c3f310364f37c723ae88b7d8f099281ae85677b
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-11 23:52:44 +0600

    set form in register component

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -2,6 +2,11 @@ const Register = () => {
   return (
     <div>
       <h3 className="text-3xl">Please Register</h3>
+      <form>
+        <input type="email" name="email" id="" />
+        <input type="password" name="password" id="" />
+        <input type="submit" value="Register" />
+      </form>
     </div>
   );
 };
