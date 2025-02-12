8aee8ca6f7c3d79dca7831cb24827fb4055d3ad7
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-12 23:56:21 +0600

    onSubmit set

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,9 +1,15 @@
 const Register = () => {
+
+  const handleRegister = e =>{
+    e.preventDefault();
+    console.log('form submitting')
+  }
+
   return (
     <div className="border">
       <div className="mx-auto md:w-1/2">
         <h3 className="text-3xl mb-8">Please Register</h3>
-        <form>
+        <form onSubmit={handleRegister}>
           <input className="mb-4 w-3/4 rounded-lg bg-gray-300 py-2 px-4" type="email" name="email" placeholder="Email Address" id="" />
           <br />
           <input className="mb-4 w-3/4 bg-gray-300 rounded-lg py-2 px-4" type="password" name="password" placeholder="Password" id="" />
