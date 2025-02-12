d6c70586e0b0a25a9ea137753182b120b518538c
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-12 23:38:23 +0600

    css add in register file

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,12 +1,16 @@
 const Register = () => {
   return (
-    <div>
-      <h3 className="text-3xl">Please Register</h3>
-      <form>
-        <input type="email" name="email" id="" />
-        <input type="password" name="password" id="" />
-        <input type="submit" value="Register" />
-      </form>
+    <div className="border">
+      <div className="mx-auto md:w-1/2">
+        <h3 className="text-3xl mb-8">Please Register</h3>
+        <form>
+          <input className="mb-4 w-3/4 rounded-lg bg-gray-300 py-2 px-4" type="email" name="email" id="" />
+          <br />
+          <input className="mb-4 w-3/4 bg-gray-300 rounded-lg py-2 px-4" type="password" name="password" id="" />
+          <br />
+          <input className="mb-4 w-3/4 btn btn-secondary" type="submit" value="Register" />
+        </form>
+      </div>
     </div>
   );
 };
--- src/main.jsx
+++ src/main.jsx
@@ -23,7 +23,7 @@ const router = createBrowserRouter([
         element: <Login></Login>,
       },
       {
-        path: "/",
+        path: "/register",
         element: <Register></Register>,
       },
     ],
