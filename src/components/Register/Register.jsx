4f1f8b2ff0caef277108b6ce153db4c1344ca9dc
Alal Hosen <alalhosen1992620120@gmail.com>
2025-02-12 23:41:30 +0600

    add placeholder

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -4,9 +4,9 @@ const Register = () => {
       <div className="mx-auto md:w-1/2">
         <h3 className="text-3xl mb-8">Please Register</h3>
         <form>
-          <input className="mb-4 w-3/4 rounded-lg bg-gray-300 py-2 px-4" type="email" name="email" id="" />
+          <input className="mb-4 w-3/4 rounded-lg bg-gray-300 py-2 px-4" type="email" name="email" placeholder="Email Address" id="" />
           <br />
-          <input className="mb-4 w-3/4 bg-gray-300 rounded-lg py-2 px-4" type="password" name="password" id="" />
+          <input className="mb-4 w-3/4 bg-gray-300 rounded-lg py-2 px-4" type="password" name="password" placeholder="Password" id="" />
           <br />
           <input className="mb-4 w-3/4 btn btn-secondary" type="submit" value="Register" />
         </form>
